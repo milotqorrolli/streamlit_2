24c7b8071168125fb4c49cdf68222f6981f9b4f4
Milot Qorrolli <118934072+milotq1@users.noreply.github.com>
2023-11-28 20:42:05 +0100

    added a button

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ rate = st.number_input('Insert rate')
 def calc_wage(hour,rate):
     return hour*rate
 
-result = calc_wage(hours, rate)
 
-calc_button = st.button('Calculate your wage')
+if st.button('Calculate your wage'):
+    result = calc_wage(hours, rate)
 
-if calc_button:
-    st.write('Your wage is: ', result)
+
+st.write('Your wage is: ', result)
