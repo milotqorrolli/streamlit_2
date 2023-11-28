02f389a41ff55179d310f7cab9687346b3a76d85
Milot Qorrolli <118934072+milotq1@users.noreply.github.com>
2023-11-28 20:33:54 +0100

    create main

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title('Hello to our wage app :sunglasses:')
+hours = st.number_input('Insert hours')
+rate = st.number_input('Insert rate')
+
+calc_button = st.button("Calculate your wage")
+
+def calc_wage(hour,rate):
+    return hour*rate
+
+result = calc_wage(hours, rate)
+
+st.write('Your wage is: ', result)
