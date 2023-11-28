e4a837a986592b022291340f7556250b57ba4a1b
Milot Qorrolli <118934072+milotq1@users.noreply.github.com>
2023-11-28 20:37:24 +0100

    #

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ st.title('Hello to our wage app :sunglasses:')
 hours = st.number_input('Insert hours')
 rate = st.number_input('Insert rate')
 
-calc_button = st.button("Calculate your wage")
-
 def calc_wage(hour,rate):
     return hour*rate
 
